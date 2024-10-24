5e0816b2521cbd4d1d17cc93eb3fa2cb87b6abe2
kevin <da_kurlzzzzz@mail.ru>
2024-10-24 18:48:06 +0300

    mute was not displayed correctly

--- .config/i3blocks/volume.sh
+++ .config/i3blocks/volume.sh
@@ -2,5 +2,11 @@
 
 printf "%s " 𝅘𝅥𝅮
 default=$(pactl get-default-sink)
+
+mute=$(pactl -f json list sinks |
+       jq -r '.[] | select(.name == "'$default'").mute')
+
+[ $mute = true ] && echo mute && exit
+
 pactl -f json list sinks |
 jq -r '.[] | select(.name == "'$default'").volume."front-left".value_percent'
