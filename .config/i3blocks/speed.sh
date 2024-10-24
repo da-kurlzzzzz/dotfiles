ba2c6e7cc9768222a1bc2bce0adc55e8e5859b73
Zarechnev <s.zarechnev@ed-it.ru>
2024-10-24 17:53:18 +0300

    rewritten for more comfortable version

--- .config/i3blocks/speed.sh
+++ .config/i3blocks/speed.sh
@@ -1,41 +1,49 @@
 #!/usr/bin/env bash
 
-rx_file=/sys/class/net/enp2s0/statistics/rx_bytes
-tx_file=/sys/class/net/enp2s0/statistics/tx_bytes
 tau=1.5
 
-to_mb()
+rx_file=/sys/class/net/enp2s0/statistics/rx_bytes
+tx_file=${rx_file/rx/tx}
+
+rx_time=$(date +%s.%N)
+tx_time=$(date +%s.%N)
+
+update_avg()
 {
-    bc <<< "$1 / 1024"
+    x_file=$1
+    x_avg=$2
+    x_old=$3
+    x_time=$4
+
+    x_cur=$(cat $x_file)
+    dx=$(bc <<< "$x_cur - $x_old")
+
+    time_cur=$(date +%s.%N)
+    dt=$(bc <<< "$time_cur - $x_time")
+
+    x_speed=$(bc <<< "$dx / $dt")
+
+    alpha=$(printf "%f" $(bc -l <<< "1 - e(-$dt / $tau)"))
+    x_avg=$(bc <<< "$alpha * $x_speed + (1 - $alpha) * $x_avg")
+
+    echo $x_avg $x_cur $time_cur
+}
+
+to_human()
+{
+    bc <<< "$1 / 1024.0"
 }
 
 rx_avg=0
 tx_avg=0
-time=$(date +%s.%N)
-rx=$(cat $rx_file)
-tx=$(cat $tx_file)
+rx_old=$(cat $rx_file)
+tx_old=$(cat $tx_file)
 
 while true
 do
-    cur_time=$(date +%s.%N)
-    dt=$(bc <<< "$cur_time - $time")
-    dt=$(awk -v t=$(date +%s.%N) "BEGIN { print t - $time }")
+    read rx_avg rx_old rx_time < <(update_avg $rx_file $rx_avg $rx_old $rx_time)
+    read tx_avg tx_old tx_time < <(update_avg $tx_file $tx_avg $tx_old $tx_time)
 
-    cur_rx=$(cat $rx_file)
-    cur_tx=$(cat $tx_file)
-    drx=$(bc <<< "$cur_rx - $rx")
-    dtx=$(bc <<< "$cur_tx - $tx")
-
-    rx_speed=$(bc <<< "$drx / $dt")
-    tx_speed=$(bc <<< "$dtx / $dt")
-    alpha=$(printf "%f" $(bc -l <<< "1 - e(-$dt / $tau)"))
-
-    rx_avg=$(bc <<< "$alpha * $rx_speed + (1 - $alpha) * $rx_avg")
-    tx_avg=$(bc <<< "$alpha * $tx_speed + (1 - $alpha) * $tx_avg")
-    time=$cur_time
-    rx=$cur_rx
-    tx=$cur_tx
-
-    echo $(to_mb $rx_avg) ∇ ∆ $(to_mb $tx_avg)
+    echo $(to_human $rx_avg) ∇ ∆ $(to_human $tx_avg)
     sleep 2
 done
