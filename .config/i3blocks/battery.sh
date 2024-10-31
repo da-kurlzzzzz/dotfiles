1b4ba680f9be0534963348d7d0d1cfafa7f9c545
kevin <da_kurlzzzzz@mail.ru>
2024-10-31 09:58:43 +0300

    removed debugging

--- .config/i3blocks/battery.sh
+++ .config/i3blocks/battery.sh
@@ -9,7 +9,3 @@ very_full=$(cat $path/energy_full_design)
 
 percent=$(bc <<< "scale=2; $now * 100 / $very_full")
 echo $use $percent
-date >> /home/kevin/battery-log
-echo $use $percent >> /home/kevin/battery-log
-# bc <<< "scale=2; $now * 100 / $very_full"
-
