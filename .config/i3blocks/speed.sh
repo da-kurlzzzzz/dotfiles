23acfec5227508dbde289b9fa45bc4c1732f3bb6
kevin <da_kurlzzzzz@mail.ru>
2025-02-26 13:35:29 +0300

    the new one-shot speed measurement by deepseek

--- .config/i3blocks/speed.sh
+++ .config/i3blocks/speed.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 tau=1.5
+state_file="/tmp/i3blocks_speed_state"
 
 check_iface() {
     new_iface=$(ip route get 8.8.8.8 | sed -E 's/.*dev (\S*).*/\1/;q')
@@ -51,14 +52,30 @@ to_human()
     awk <<< "$1" '{print(int($1 / 1024.0))}'
 }
 
-reset_configs
+if [ -f "$state_file" ]; then
+    source "$state_file"
+else
+    reset_configs
+fi
 
-while true
-do
-    check_iface || { reset_configs; sleep 0.1; }
-    read rx_avg rx_old rx_time < <(update_avg $rx_file $rx_avg $rx_old $rx_time)
-    read tx_avg tx_old tx_time < <(update_avg $tx_file $tx_avg $tx_old $tx_time)
+if ! check_iface; then
+    reset_configs
+fi
 
-    echo $(to_human $rx_avg) ∇ ∆ $(to_human $tx_avg)
-    sleep 2
-done
+read -r rx_avg rx_old rx_time < <(update_avg $rx_file $rx_avg $rx_old $rx_time)
+read -r tx_avg tx_old tx_time < <(update_avg $tx_file $tx_avg $tx_old $tx_time)
+
+echo $(to_human $rx_avg) ∇ ∆ $(to_human $tx_avg)
+
+# Save the current state to the state file
+cat > $state_file <<EOF
+iface=$iface
+rx_file=$rx_file
+tx_file=$tx_file
+rx_avg=$rx_avg
+tx_avg=$tx_avg
+rx_old=$rx_old
+tx_old=$tx_old
+rx_time=$rx_time
+tx_time=$tx_time
+EOF
