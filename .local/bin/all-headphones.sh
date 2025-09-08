38c2f90a422768bb3b272b1bac338708336f523d
kevin <da_kurlzzzzz@mail.ru>
2025-09-08 11:57:24 +0300

    a cool way to output sound to multiple headphones

---
+++ .local/bin/all-headphones.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -x
+
+for d in $(bluetoothctl devices Paired | grep '^Device' | cut -d' ' -f2)
+do
+    if bluetoothctl info $d | grep headphones &>/dev/null
+    then
+        bluetoothctl -t 2 connect $d
+    fi
+done
+
+sinks=""
+sep=""
+for d in $(bluetoothctl devices Connected | grep '^Device' | cut -d' ' -f2)
+do
+    echo $d
+    sink="bluez_output.$(tr ':' '_' <<< $d).1"
+    pactl set-sink-mute "$sink" false
+    pactl set-sink-volume "$sink" 100%
+    sinks="${sinks}${sep}${sink}"
+    echo "$sinks"
+    [ -z "$sep" ] && sep=","
+done
+
+pactl unload-module module-combine-sink
+pactl load-module module-combine-sink "sinks=$sinks"
+pactl set-default-sink combined
