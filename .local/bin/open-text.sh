965f896ac6f2a82e6d3dadd06f9edbb386a75e3d
kevin <da_kurlzzzzz@mail.ru>
2025-11-17 14:35:26 +0300

    added mimeapps and all the relevant things

---
+++ .local/bin/open-text.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pane=$(tmux display-message -p "#S:#I.#P")
+vim_server_pid=$(pgrep -f "servername $pane")
+cmd=
+[ -z "$vim_server_pid" ] && cmd="tmux split-window -l 85%"
+$cmd vim --servername $pane --remote-tab-silent "$@"
+tmux select-pane -t $(awk < /proc/$vim_server_pid/environ -F= 'BEGIN{RS="\0"} /^TMUX_PANE=/{print $2}')
