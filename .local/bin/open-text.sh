60379d47a5774e4694a9b34c652f6181ddf4fdca
kevin <da_kurlzzzzz@mail.ru>
2025-12-08 16:55:27 +0300

    tmux already selects new pane by itself

--- .local/bin/open-text.sh
+++ .local/bin/open-text.sh
@@ -4,5 +4,4 @@ pane=$(tmux display-message -p "#S:#I.#P")
 [ -z "$pane" ] || vim_server_pid=$(pgrep -f "servername $pane")
 [ -z "$vim_server_pid" ] && cmd="tmux split-window -l 80%"
 $cmd vim --servername $pane --remote-tab-silent "$@"
-vim_server_pid=$(pgrep -f "servername $pane")
-tmux select-pane -t $(awk < /proc/$vim_server_pid/environ -F= 'BEGIN{RS="\0"} /^TMUX_PANE=/{print $2}')
+[ -z "$vim_server_pid" ] || tmux select-pane -t $(awk < /proc/$vim_server_pid/environ -F= 'BEGIN{RS="\0"} /^TMUX_PANE=/{print $2}')
