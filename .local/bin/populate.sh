61dea81709ca8275747b6439e675531beaf4751c
kevin <da_kurlzzzzz@mail.ru>
2026-01-14 19:23:14 +0300

    moved ssh population to a separate script

---
+++ .local/bin/populate.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -xeu
+
+archive=$HOME/.cache/archive.tar
+
+populate_files=(.aliasrc .bash_profile .bashrc .config/htop/htoprc .config/ranger/rc.conf .config/ranger/rifle.conf .envrc .hushlogin .inputrc .profile .ssh/config .vim/defaults.vim .vim/keymap/russian-jcukenwin-custom.vim .vim/pack/tpope/start/sensible/plugin/sensible.vim .vim/skel/c .vim/skel/cpp .vim/skel/html .vim/skel/markdown .vim/skel/python .vim/skel/sh .vim/vimrc)
+
+usage()
+{
+    echo "Usage: $0 <command> [arguments]"
+    echo "supported commands:"
+    echo "  ssh <host>"
+}
+
+[ "$#" -lt 1 ] && { usage; exit 1; }
+[ -z "$1" ] && { usage; exit 1; }
+
+[ "$1" = "ssh" ] || { usage; exit 2; }
+[ "$#" -lt 2 ] && { usage; exit 2; }
+[ -z "$2" ] && { usage; exit 2; }
+host="$2"
+
+pushd $HOME
+tar -cf $archive ${populate_files[@]}
+ln -sf ../../.ranger/ranger.py $HOME/.local/bin/ranger
+tar -rf $archive .ranger .local/bin/ranger
+rm .local/bin/ranger
+gzip -f $archive
+archive="$archive.gz"
+ssh $host mkdir -p .cache
+scp -O $archive $host:.cache
+ssh $host tar -xf .cache/$(basename $archive)
+popd
