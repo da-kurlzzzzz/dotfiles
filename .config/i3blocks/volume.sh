96a644631acf9528ed64b5277507aeec9fc74108
kevin <da_kurlzzzzz@mail.ru>
2026-02-09 19:24:10 +0300

    fix for a missing parameter

--- .config/i3blocks/volume.sh
+++ .config/i3blocks/volume.sh
@@ -5,7 +5,7 @@ default=$(pactl get-default-sink)
 mute=$(pactl -f json list sinks |
        jq -r '.[] | select(.name == "'$default'").mute')
 
-[ $mute = true ] && echo mute && exit
+[ "$mute" = true ] && echo mute && exit
 
 pactl -f json list sinks |
 jq -r '.[] | select(.name == "'$default'").volume | map(select(has("value_percent")))[0].value_percent'
