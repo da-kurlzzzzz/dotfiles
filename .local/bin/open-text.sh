99a22c0c4df71426ce225602784b9780efd582c0
kevin <da_kurlzzzzz@mail.ru>
2026-02-17 12:06:00 +0300

    clientserver vim when connected over ssh

--- .local/bin/open-text.sh
+++ .local/bin/open-text.sh
@@ -37,7 +37,7 @@ elif is_tmux_available; then
     pane=$(tmux display-message -p "#S:#I.#P")
     [ -z "$pane" ] || vim_server_pid=$(pgrep -f "servername $pane")
     [ -z "$vim_server_pid" ] && cmd="tmux split-window -l 80%"
-    $cmd vim --servername $pane --remote-tab-silent "$@"
+    $cmd vim --clientserver socket --servername $pane --remote-tab-silent "$@"
     [ -z "$vim_server_pid" ] || tmux select-pane -t $(awk < /proc/$vim_server_pid/environ -F= 'BEGIN{RS="\0"} /^TMUX_PANE=/{print $2}')
 else
     vim "$@"
