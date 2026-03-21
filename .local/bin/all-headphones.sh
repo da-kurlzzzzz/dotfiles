c69a79b1811d50d586b6387010737b19bf4ec1f8
kevin <da_kurlzzzzz@mail.ru>
2026-03-21 13:49:06 +0300

    bluetoothctl changed interface again

--- .local/bin/all-headphones.sh
+++ .local/bin/all-headphones.sh
@@ -2,19 +2,19 @@
 
 set -x
 
-for d in $(bluetoothctl devices Paired | grep '^Device' | cut -d' ' -f2)
+for d in $(bluetoothctl <<< "devices Paired" | grep '^Device' | cut -d' ' -f2)
 do
-    if bluetoothctl info $d | grep -i audio &>/dev/null
+    if bluetoothctl <<< "info $d" | grep -i audio &>/dev/null
     then
-        bluetoothctl -t 2 connect $d
+        bluetoothctl -t 2 <<< "connect $d"
     fi
 done
 
 sinks=""
 sep=""
-for d in $(bluetoothctl devices Connected | grep '^Device' | cut -d' ' -f2)
+for d in $(bluetoothctl <<< "devices Connected" | grep '^Device' | cut -d' ' -f2)
 do
-    echo $d
+    echo "$d"
     sink="bluez_output.$d"
     pactl set-sink-mute "$sink" false
     pactl set-sink-volume "$sink" 100%
